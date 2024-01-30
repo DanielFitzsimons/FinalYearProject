858e965aa5b0f870b636ea9bb24fa8c61e76061a
DanielFitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2024-01-30 16:17:40 +0000

    extracted interfaces to seperate files for reusability

---
+++ src/app/models/model/model.ts
@@ -0,0 +1,29 @@
+import { Timestamp } from "@angular/fire/firestore";
+
+// models.ts
+
+export interface User {
+    id: string;
+    name: string;
+    // ... other user properties ...
+  }
+  
+ 
+  
+  export interface Conversation {
+    id: string;
+    participantIds: string[];
+    lastMessage: string;
+    lastMessageTimestamp: Date;
+    otherUserName?: string;
+    // ... other conversation properties ...
+  }
+
+  export interface Message {
+    id?: string;
+    conversationId: string;
+    sender: string;
+    content: string;
+    timestamp: Timestamp;
+  }
+  
